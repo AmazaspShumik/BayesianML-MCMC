638253e5c86744a216c5e51052aab9bddb7fd580
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-05-27 16:22:54 +0300

    better example

--- Gibbs Bernoulli Mixture/gibbsBernoulliMixtureExample.m
+++ Gibbs Bernoulli Mixture/gibbsBernoulliMixtureExample.m
@@ -1,19 +1,44 @@
-% % TEST 1
-% x = zeros(100,3);
-% p1 = [0.001,0.99999,0.001];
-% p2 = [0.999,0.001,0.999];
-% for i = 1:50
-%     x(i,:) = binornd(1,p1);
-%     x(50+i,:) = binornd(1,p2);
-% end
-% [samples,clusters,logLike] = collapsedGibbsBernoulliMixture(x,2,20,1000,2);
-% [Samples,Clusters,LogLike] = vanillaGibbsBernoulliMixture(x,2,20,1000,2);
+% This script illustrates use of Bernoulli Mixture 
+%
+% Data used in example is random sample from subset of data provided
+% in digit recognition competition in kaggle
+% We use only three digits : 1,6,3 (100 data points for each of them)
 
-
+% load data 
 X = csvread('digits_examples.csv');
-% vanilla Gibbs
-%[vMuSamples,vClusters,vLogLike] = vanillaGibbsBernoulliMixture(X,3,10,1000,2);
-[cMuSamples,cClusters,cLogLike] = collapsedGibbsBernoulliMixture(X,3,10,100,2);
-plot(vLogLike,'r-')
+
+% parameters of model
+nComponents = 3;
+nSamples    = 10;
+nBurnin     = 20;
+nThin       = 2;
+logLikeCompute = true; % collapsedGibbs should be faster without this
+
+% vanilla Gibbs Sample
+[vMuSamples,vClusters,vLogLike] = vanillaGibbsBernoulliMixture(X,nComponents,...
+                                  nSamples,nBurnin,nThin,logLikeCompute);
+% collapsed Gibbs Sample
+[cMuSamples,cClusters,cLogLike] = collapsedGibbsBernoulliMixture(X,nComponents,...
+                                  nSamples,nBurnin,nThin,logLikeCompute);
+
+% show all three components from last sample 
+for j = 1:nComponents
+    figure(j)
+    imshow(reshape(sign(vMuSamples(j,:,nSamples)-0.5),28,28)')
+    title('Sample component mean, vanilla Gibbs')
+    figure(j+nComponents)
+    imshow(reshape(sign(cMuSamples(j,:,nSamples)-0.5),28,28)')
+    title('Sample component mean, collapsed Gibbs')
+end
+
+% plot log-likelihoods, show that collapsedGibbs converges faster
+figure(2*nComponents+1)
+plot(vLogLike,'b-')
 hold on
-plot(cLogLike,'g-')
+plot(cLogLike,'r-')
+xlabel('Iterations')
+ylabel('log-likelihood')
+legend('vanillaGibbs','collapsedGibbs','Location','southeast')
+title('Convergence coparison for Gibbs Sampler: collapsed vs vanilla')
+
+    
